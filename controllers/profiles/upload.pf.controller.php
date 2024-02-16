b14dba9c260da1746155cd4dbcbd2ca5cd7d1b4d
ROTH SAMRETH <rothsamreth@student.passerellesnumeriques.org>
2024-02-16 19:16:48 +0700

    [feat/upload profile] finalize feature  GL-13

--- controllers/profiles/upload.pf.controller.php
+++ controllers/profiles/upload.pf.controller.php
@@ -1,11 +1,9 @@
 <?php
 require "../../database/database.php";
 require "../../models/profile.model.php";
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $file = $_FILES['file'];
     $uid = $_POST['uid'];
-    echo $uid;
     $filename = $_FILES['file']['name'];
     $filetmpName = $_FILES['file']['tmp_name'];
     $fileSize = $_FILES['file']['size'];
@@ -20,17 +18,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $filenewName = uniqid('', true) . "." . $fileActualExt;
                 $directory = '../../assets/profile/profiles' . $filenewName;
                 $newdirect = 'assets/profile/profiles' . $filenewName;
-                if (updateProfile($uid, $newdirect)) {
-                    move_uploaded_file($filetmpName, $directory);
-                    header("location : /manages");
+                if ($uid !== '') {
+                    if (updateProfile($uid, $newdirect)) {
+                        move_uploaded_file($filetmpName, $directory);
+                        header('location: /manages');
+                    }
+                } else {
+                    header('location: /profileImage?user=notfound');
                 }
             } else {
+                header('location: /profileImage?filesize=large');
                 echo "You file size is too large";
             }
         } else {
             echo "An upload error have been accurred";
+            header('location: /profileImage?error=uploaderror');
         }
     } else {
         echo "You are not allow to upload this file";
+        header('location: /profileImage?file=unsupported');
     }
 }
--- views/profiles/profile.upload.view.php
+++ views/profiles/profile.upload.view.php
@@ -15,7 +15,9 @@
                         <!-- Form -->
                         <form action="controllers/profiles/upload.pf.controller.php" method="post" enctype="multipart/form-data">
                             <div class="form-group">
-                                <input class="form-control" type="hidden" name="uid" value="<?= $_GET['id'] ?>">
+                                <input class="form-control" type="hidden" name="uid" value="<?php if (isset($_GET['id'])) {
+                                                                                                echo $_GET['id'];
+                                                                                            } ?>">
                             </div>
                             <div class="form-group">
                                 <input class="form-control" type="file" name="file">
