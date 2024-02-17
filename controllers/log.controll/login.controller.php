e01c7db191580f1ca722f73c843089e4633367e0
KHLORP.VEAK <KHLORP.VEAK@pnc.lan>
2024-02-17 08:36:31 +0700

    login user #GL23

--- controllers/log.controll/login.controller.php
+++ controllers/log.controll/login.controller.php
@@ -1,2 +1,30 @@
 <?php
-header("location: /admin");
+// header("location: /admin");
+session_start();
+require '../../database/database.php';
+require '../../models/employee.model.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    $email = htmlspecialchars($_POST['email']);
+    $password = htmlspecialchars($_POST['password']);//123
+
+    // Get data from database
+    $user = accountExist($email);
+    // Check if user exists
+    if (count($user) > 0) {
+        // Check if password is correct
+        if (password_verify($password, $user["password"])) {
+            echo "Password is incorrect";
+        } else {
+            $_SESSION['user'] = $user;
+            header('Location: /employees');
+        }
+    } 
+    else {
+        echo "No user found";
+    }
+}
+
+
+
--- models/employee.model.php
+++ models/employee.model.php
@@ -50,3 +50,30 @@ function deletePost(int $id) : bool
     $statement->execute([':id' => $id]);
     return $statement->rowCount() > 0;
 }
+
+//login
+function createAccount(string $name, string $email, string $password): bool {
+    global $connection;
+    $statement = $connection->prepare("INSERT INTO users (name, email, password, role) VALUES (:name, :email, :password, :role)");
+    $statement->execute([
+        ':name'=>$name,
+        ':email'=>$email,
+        ':password'=>$password,
+        ':role'=>"user"
+    ]);
+    return $statement->rowCount() > 0;
+};
+
+//check email
+function accountExist(string $email): array {
+    global $connection;
+    $statement = $connection->prepare("SELECT * FROM users WHERE email = :email");
+    $statement -> execute([':email'=>$email]);
+    // return $statement->fetch();
+    if($statement->rowCount() > 0){
+        return $statement -> fetch();
+
+    }else{
+        return [];
+    }
+}
