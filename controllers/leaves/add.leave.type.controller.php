93447a0cec7d0dd5af8e36345c43a6da31b9a681
NEARDEY.LOEM <NEARDEY.LOEM@PNC.LAN>
2024-02-17 12:42:08 +0700

    feat/add_leave.type] redirect to leave type page #GL-46

--- controllers/leaves/add.leave.type.controller.php
+++ controllers/leaves/add.leave.type.controller.php
@@ -1,13 +1,17 @@
-<?php 
+<?php
 require "../../database/database.php";
 require "../../models/leavetype.model.php";
-if($_SERVER['REQUEST_METHOD']=="POST"){
-    $typename=$_POST['typename'];
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $typename = $_POST['typename'];
     // $typedesc=$_POST['typedesc'];
-    $iscreated=addLeaveType($typename);
-    if($iscreated){
-        header("location: /leavetype");
-    }else{
+    if ($typename !== '') {
+        $iscreated = addLeaveType($typename);
+        if ($iscreated) {
+            header("location: /leavetype");
+        } else {
+            header("location: /leavetypeForm");
+        }
+    }else {
         header("location: /leavetypeForm");
     }
 }
