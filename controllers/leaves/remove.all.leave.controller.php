39cea761012a941da8610c39f3e0a33eff16926e
ROTH SAMRETH <rothsamreth@student.passerellesnumeriques.org>
2024-02-21 20:33:32 +0700

    [feat/delete_all_leave] add code on all leave request removal feature GL-49

--- controllers/leaves/remove.all.leave.controller.php
+++ controllers/leaves/remove.all.leave.controller.php
@@ -3,8 +3,11 @@ require "../../database/database.php";
 require "../../models/leave_request.model.php";
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['confirm']) and $_POST['confirm'] !== "" and $_POST['confirm'] === "Yes I am sure.") {
-        removeAll();
-        header("location: /leaves");
+        if (removeAll()) {
+            header("location: /leaves?msg=Successfully%20removed");
+        } else {
+            header("location: /leaves?err=Failed%20to%20");
+        }
     } else {
         header("location: /leaves?error=canceled");
     }
