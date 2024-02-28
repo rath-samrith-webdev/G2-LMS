b2aca8858e411fd7bf8aa0ac675f4fcc9c66c81e
ROTH SAMRETH <rothsamreth@student.passerellesnumeriques.org>
2024-02-28 20:27:54 +0700

    [feat] intialize show request on calendar feature

--- controllers/calendars/calendar.controller.php
+++ controllers/calendars/calendar.controller.php
@@ -1,3 +1,5 @@
 <?php
-
+require "database/database.php";
+require "models/leave_request.model.php";
+$leaverequest = getALlleaves();
 require "views/calendars/calendar.view.php";
--- views/calendars/calendar.view.php
+++ views/calendars/calendar.view.php
@@ -121,41 +121,198 @@ require "layouts/navbar.php"; ?>
 		</div>
 	</div>
 </div>
+<?php require "layouts/footer.php" ?>
+<script>
+	!(function($) {
+		"use strict";
 
-<!-- <div class="sidebar-overlay" id="sidebar_overlay"></div> -->
-<!-- jQuery -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-<script src="vendor/js/jquery-3.2.1.min.js"></script>
+		var CalendarApp = function() {
+			this.$body = $("body");
+			(this.$calendar = $("#calendar")),
+			(this.$event = "#calendar-events div.calendar-events"),
+			(this.$categoryForm = $("#add_new_event form")),
+			(this.$extEvents = $("#calendar-events")),
+			(this.$modal = $("#my_event")),
+			(this.$saveCategoryBtn = $(".save-category")),
+			(this.$calendarObj = null);
+		};
+		/* on click on event */
+		(CalendarApp.prototype.onEventClick = function(calEvent, jsEvent, view) {
+			var $this = this;
+			var form = $("<form></form>");
+			form.append("<label>Change event name</label>");
+			form.append(
+				"<div class='input-group'><input class='form-control' type=text value='" +
+				calEvent.title +
+				"' /><span class='input-group-append'><button type='submit' class='btn btn-success'><i class='fa fa-check'></i> Save</button></span></div>"
+			);
+			$this.$modal.modal({
+				backdrop: "static",
+			});
+			$this.$modal
+				.find(".delete-event")
+				.show()
+				.end()
+				.find(".save-event")
+				.hide()
+				.end()
+				.find(".modal-body")
+				.empty()
+				.prepend(form)
+				.end()
+				.find(".delete-event")
+				.unbind("click")
+				.click(function() {
+					$this.$calendarObj.fullCalendar("removeEvents", function(ev) {
+						return ev._id == calEvent._id;
+					});
+					$this.$modal.modal("hide");
+				});
+			$this.$modal.find("form").on("submit", function() {
+				calEvent.title = form.find("input[type=text]").val();
+				$this.$calendarObj.fullCalendar("updateEvent", calEvent);
+				$this.$modal.modal("hide");
+				return false;
+			});
+		}),
+		/* on select */
+		(CalendarApp.prototype.onSelect = function(start, end, allDay) {
+			var $this = this;
+			$this.$modal.modal({
+				backdrop: "static",
+			});
+			var form = $("<form></form>");
+			form.append("<div class='event-inputs'></div>");
+			form
+				.find(".event-inputs")
+				.append(
+					"<div class='form-group'><label class='control-label'>Event Name</label><input class='form-control' placeholder='Insert Event Name' type='text' name='title'/></div>"
+				)
+				.append(
+					"<div class='form-group'><label class='control-label'>Category</label><select class='form-control' name='category'></select></div>"
+				)
+				.find("select[name='category']")
+				.append("<option value='bg-danger'>Danger</option>")
+				.append("<option value='bg-success'>Success</option>")
+				.append("<option value='bg-purple'>Purple</option>")
+				.append("<option value='bg-primary'>Primary</option>")
+				.append("<option value='bg-info'>Info</option>")
+				.append("<option value='bg-warning'>Warning</option></div></div>");
+			$this.$modal
+				.find(".delete-event")
+				.hide()
+				.end()
+				.find(".save-event")
+				.show()
+				.end()
+				.find(".modal-body")
+				.empty()
+				.prepend(form)
+				.end()
+				.find(".save-event")
+				.unbind("click")
+				.click(function() {
+					form.submit();
+				});
+			$this.$modal.find("form").on("submit", function() {
+				var title = form.find("input[name='title']").val();
+				var beginning = form.find("input[name='beginning']").val();
+				var ending = form.find("input[name='ending']").val();
+				var categoryClass = form
+					.find("select[name='category'] option:checked")
+					.val();
+				if (title !== null && title.length != 0) {
+					$this.$calendarObj.fullCalendar(
+						"renderEvent", {
+							title: title,
+							start: start,
+							end: end,
+							allDay: false,
+							className: categoryClass,
+						},
+						true
+					);
+					$this.$modal.modal("hide");
+				} else {
+					alert("You have to give a title to your event");
+				}
+				return false;
+			});
+			$this.$calendarObj.fullCalendar("unselect");
+		}),
+		(CalendarApp.prototype.enableDrag = function() {
+			//init events
+			$(this.$event).each(function() {
+				// it doesn't need to have a start or end
+				var eventObject = {
+					title: $.trim($(this).text()), // use the element's text as the event title
+				};
+				// store the Event Object in the DOM element so we can get to it later
+				$(this).data("eventObject", eventObject);
+				// make the event draggable using jQuery UI
+				$(this).draggable({
+					zIndex: 999,
+					revert: true, // will cause the event to go back to its
+					revertDuration: 0, //  original position after the drag
+				});
+			});
+		});
+		/* Initializing */
+		(CalendarApp.prototype.init = function() {
+			this.enableDrag();
+			/*  Initialize the calendar  */
+			var date = new Date();
+			var d = date.getDate();
+			var m = date.getMonth();
+			var y = date.getFullYear();
+			var form = "";
+			var today = new Date($.now());
 
-<!-- Bootstrap Core JS -->
-<script src="vendor/js/popper.min.js"></script>
-<script src="vendor/js/bootstrap.min.js"></script>
+			var defaultEvents = [
+				<?php foreach ($leaverequest as $request) { ?> {
+						title: "<?= $request['first_name'] ?>",
+						start: "<?= $request['start_date'] ?>",
+						className: "bg-purple",
+					},
+				<?php } ?>
+			];
 
-<!-- Chart JS -->
-<script src="vendor/js/Chart.min.js"></script>
-<script src="vendor/js/chart.js"></script>
+			var $this = this;
+			$this.$calendarObj = $this.$calendar.fullCalendar({
+				slotDuration: "00:15:00" /* If we want to split day time each 15minutes */ ,
+				minTime: "08:00:00",
+				maxTime: "19:00:00",
+				defaultView: "month",
+				handleWindowResize: true,
 
-<!-- Datetimepicker JS -->
-<script src="vendor/plugins/select2/moment.min.js"></script>
-<script src="vendor/js/bootstrap-datetimepicker.min.js"></script>
-
-<!-- Select2 JS -->
-<script src="vendor/plugins/select2/select2.min.js"></script>
-
-<!-- Full Calendar JS -->
-<script src="vendor/js/jquery-ui.min.js"></script>
-
-<script src="vendor/plugins/fullcalendar/fullcalendar.min.js"></script>
-<script src="vendor/plugins/fullcalendar/jquery.fullcalendar.js"></script>
-
-<!-- Sticky sidebar JS -->
-<script src="vendor/plugins/theia-sticky-sidebar/ResizeSensor.js"></script>
-<script src="vendor/plugins/theia-sticky-sidebar/theia-sticky-sidebar.js"></script>
-
-<!-- Custom Js -->
-<script src="vendor/js/script.js"></script>
-
-</body>
-
-</html>
+				header: {
+					left: "prev,next today",
+					center: "title",
+					right: "month,agendaWeek,agendaDay",
+				},
+				events: defaultEvents,
+				editable: true,
+				droppable: true, // this allows things to be dropped onto the calendar !!!
+				eventLimit: true, // allow "more" link when too many events
+				selectable: true,
+				drop: function(date) {
+					$this.onDrop($(this), date);
+				},
+				select: function(start, end, allDay) {
+					$this.onSelect(start, end, allDay);
+				},
+				eventClick: function(calEvent, jsEvent, view) {
+					$this.onEventClick(calEvent, jsEvent, view);
+				},
+			});
+		}),
+		//init CalendarApp
+		($.CalendarApp = new CalendarApp()),
+		($.CalendarApp.Constructor = CalendarApp);
+	})(window.jQuery),
+	//initializing CalendarApp
+	(function($) {
+		"use strict";
+		$.CalendarApp.init();
+	})(window.jQuery);
+</script>
