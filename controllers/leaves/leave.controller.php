0ba1fc3c0bff673b72db63085f0648841c74f61f
ROTH SAMRETH <rothsamreth@student.passerellesnumeriques.org>
2024-03-02 11:46:25 +0700

    [feat] intailize feature GL-78

--- controllers/leaves/leave.controller.php
+++ controllers/leaves/leave.controller.php
@@ -1,12 +1,14 @@
 <?php
 require "database/database.php";
 require "models/leave_request.model.php";
+require "models/admin.model.php";
 
 // ======== Capture function leave request model =========
-$leaves=getLeaveData();
-$leave_requests=getALlleaves();
-
+$leaves = getLeaveData();
+if (isset($_SESSION['user']['uid'])) {
+    $id = $_SESSION['user']['uid'];
+    $leave_requests = getPersonalLeaves($id);
+} else {
+    $leave_requests = getALlleaves();
+}
 require "views/leaves/leave.view.php";
-
-
-
