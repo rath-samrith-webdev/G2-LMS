ed367d39b31f4b6b0340d8e449b92d7a46c60d4c
ROTH SAMRETH <rothsamreth@student.passerellesnumeriques.org>
2024-03-02 11:53:51 +0700

    [feat] get user id and the user's requests from database GL-78

--- controllers/leaves/leave.controller.php
+++ controllers/leaves/leave.controller.php
@@ -5,10 +5,10 @@ require "models/admin.model.php";
 
 // ======== Capture function leave request model =========
 $leaves = getLeaveData();
-if (isset($_SESSION['user']['uid'])) {
+if (isset($_SESSION['user']['uid']) and isset($_SESSION['user']['email'])) {
     $id = $_SESSION['user']['uid'];
-    $leave_requests = getPersonalLeaves($id);
+    $leave_requests = getPersonalLeaves($id); // Get the leaves of current user from database
 } else {
-    $leave_requests = getALlleaves();
+    $leave_requests = getALlleaves(); //get all request for admin
 }
 require "views/leaves/leave.view.php";
