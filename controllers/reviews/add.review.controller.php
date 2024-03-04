f54e3f0a075f88373d7bf2df300ee4cf8f2916b0
Radit They <rdit.thy@student.passerellesnumeriques.org>
2024-03-04 19:48:37 +0700

    [imp]edit status as a user GL-123

--- controllers/reviews/add.review.controller.php
+++ controllers/reviews/add.review.controller.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $reviewType_id = $_POST['type'];    //review type id
     $start_date = $_POST['start_date']; //start date
     $end_date = $_POST['end_date'];     //end date
-    $uid = $_SESSION['user']['uid'];    //user id
+    $uid = $_SESSION['user']['uid'];    ///user id
 
     if ($uid !== '' and $reviewType_id !== '' and $start_date !== '' and $end_date !== '') {
         $add = postReviewData($uid, $reviewType_id, $start_date, $end_date);
