84784a18cd6f1332cd351c24ae0f32b071ee9f18
KHLORP.VEAK <KHLORP.VEAK@PNC.LAN>
2024-03-06 08:01:31 +0700

    [feat] update user login

--- views/landing/login.view.php
+++ views/landing/login.view.php
@@ -1,27 +1,55 @@
 <?php require "layouts/header.php"?>
-<!-- Main Wrapper -->
+<!-- Bootstrap 5 -->
+<link rel="stylesheet" href="/path/to/cdn/bootstrap.min.css" />
+<script src="/path/to/cdn/bootstrap.bundle.min.js"></script>
+<!-- Icon Picker -->
+<script src="/path/to/dist/iconpicker.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+
 <div class="inner-wrapper login-body">
     <div class="login-wrapper">
+        <div class="p-5 bg-image" style="
+            background-image: url('https://mdbootstrap.com/img/new/textures/full/171.jpg');
+            height: 200px;
+            "></div>
         <div class="container">
-            <div class="loginbox shadow-sm grow">
+            <div class="loginbox shadow-sm " style="
+                margin-top: -100px;
+                background: hsla(0, 0%, 100%, 0.8);
+                backdrop-filter: blur(30px);
+                ">
                 <div class="login-left">
                     <img class="img-fluid" src="views/landing/image.login.views.png" alt="logo">
                 </div>
                 <div class="login-right">
                     <div class="login-right-wrap">
-                        <h1>Login</h1>
+                        <h1><b>Login</b></h1>
                         <p class="account-subtitle">Access to your dashboard</p>
 
                         <!-- Form -->
                         <form action="controllers/log.controll/login.controller.php" method="post">
-                            <?php if(isset($_GET['error'])) {?>
-                                <p class="alert alert-danger text-danger" role="alert"><?= $_GET['error'];?></p>
-                            <?php } ?>
                             <div class="form-group">
-                                <input class="form-control" type="email" placeholder="User Email" name="email" required>
+                                <?php if(isset($_GET['error'])) {?>
+                                    <p class="alert alert-danger text-danger" role="alert"><?= $_GET['error'];?></p>
+                                <?php } ?>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text h-100 selected-icon">
+                                            <i class="fa fa-envelope-o"></i> 
+                                        </span>
+                                    </div>
+                                    <input class="form-control" type="text" placeholder="User Email" name="email" required>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <input class="form-control" type="text" placeholder="Password" name="password">
+                            <div class="form-group">                                    
+                                <div class="input-group" id="show_hide_password">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text h-100 selected-icon">
+                                            <a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>
+                                        </span>
+                                    </div>
+                                    <input class="form-control" type="password" placeholder="Password" name="password">                                        
+                                </div>
                             </div>
                             <div class="form-group">
                                 <button class="btn btn-theme button-1 text-white ctm-border-radius btn-block" type="submit">Login</button>
