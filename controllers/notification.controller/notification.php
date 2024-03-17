4d209558e6ef5f0645d5b4453b4b734d73f2ea4f
ROTH SAMRETH <rothsamreth@student.passerellesnumeriques.org>
2024-03-17 17:05:48 +0700

    [feat] change codes for the manager to be able to appprove and reject and user to be able to the details GL-127

--- controllers/notification.controller/notification.php
+++ controllers/notification.controller/notification.php
@@ -5,19 +5,26 @@ if (isset($_SESSION['user']['uid'])) {
   if (isset($_POST['view'])) {
     $con = mysqli_connect("localhost", "root", "", "leave_manage_db");
     $uid = $_SESSION['user']['uid'];
-    $query = "SELECT * FROM total_requests WHERE uid=" . $uid . " ORDER BY request_id DESC LIMIT 5";
+    $role_id = $_SESSION['user']['role_id'];
+    $dept_id = $_SESSION['user']['department_id'];
+    if ($role_id == 1) {
+      $query = "SELECT * FROM (((leave_requests INNER JOIN users ON leave_requests.uid=users.uid)INNER JOIN leave_status ON leave_requests.status_id=leave_status.status_id)INNER JOIN leave_types ON leave_requests.leavetype_id=leave_types.leaveType_id) WHERE department_id=" . $dept_id . " ORDER BY request_id DESC LIMIT 5";
+    } else {
+      $query = "SELECT * FROM total_requests WHERE uid=" . $uid . " ORDER BY request_id DESC LIMIT 5";
+    }
     $result = mysqli_query($con, $query);
     $output = '';
     if (mysqli_num_rows($result) > 0) {
       while ($row = mysqli_fetch_array($result)) {
-        if ($row['uid'] = $uid) {
+        if ($row['uid'] == $uid) {
+          $name = "You";
           $output .= '
                 <div class="notice-board">
                 <div class="table-img">
-                  <div class="e-avatar mr-3"><img class="img-fluid" src="' . $row['profile'] . '" alt="' . $row['first_name'] . 'Danny Ward"></div>
+                  <div class="e-avatar mr-3"><img class="img-fluid" src="' . $row['profile'] . '" alt="' . $row['first_name'] . '"></div>
                 </div>
                 <div class="notice-body">
-                  <h6 class="mb-0">You have requested for ' . $row['leaveType_desc'] . ' .</h6>
+                  <h6 class="mb-0">' . $name . ' have requested for ' . $row['leaveType_desc'] . ' .</h6>
                   <span class="ctm-text-sm">' . $row['first_name'] . " " . $row['last_name'] . ' | <b>' . hm_time_ago($row['added_times']) . '</b></span>
                 </div>
               </div>
@@ -29,7 +36,7 @@ if (isset($_SESSION['user']['uid'])) {
                   <div class="e-avatar mr-3"><img class="img-fluid" src="' . $row['profile'] . '" alt="' . $row['first_name'] . 'Danny Ward"></div>
                 </div>
                 <div class="notice-body">
-                  <h6 class="mb-0">' . $row['first_name'] . 'requested for ' . $row['leaveType_desc'] . ' .</h6>
+                  <h6 class="mb-0">' . $row['first_name'] . ' has requested for ' . $row['leaveType_desc'] . ' .</h6>
                   <span class="ctm-text-sm">' . $row['first_name'] . " " . $row['last_name'] . ' | <b>' . hm_time_ago($row['added_times']) . '</b></span>
                 </div>
               </div>
@@ -45,9 +52,6 @@ if (isset($_SESSION['user']['uid'])) {
     </div>
     <hr>';
     }
-
-
-
     $status_query = "SELECT * FROM total_requests WHERE status_desc='Pending'";
     $result_query = mysqli_query($con, $status_query);
     $count = mysqli_num_rows($result_query);
