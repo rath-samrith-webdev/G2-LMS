6d124d51af72b7477872135584fa497c036bcccb
ROTH SAMRETH <rothsamreth@student.passerellesnumeriques.org>
2024-03-27 12:00:07 +0700

    [imp] prevent user from entering the wrong date

--- controllers/leaves/add.leave.controller.php
+++ controllers/leaves/add.leave.controller.php
@@ -8,7 +8,9 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $type = $_POST['leave_type'];
     $start = $_POST['start_date'];
     $end = $_POST['end_date'];
+    $diff = date_diff($date1, $date2);
     $leaveRemain = $_SESSION['user']['total_allowed_leave'];
+    $total = $leaveRemain - $diff->format("%a");
     if (date('Y', strtotime($start)) > date('Y', strtotime($end))) {
         header("location: /calendars?leaveerror=notvalid");
     } elseif (date('m', strtotime($start)) > date('m', strtotime($end))) {
